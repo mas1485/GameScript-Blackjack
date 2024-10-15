b5bb414bc538e359871932ad1c99f5c5dad0eac2
Matthew Scott <matthew.scott@students.codeinstitute.net>
2024-10-15 18:34:46 +0100

    footer
    changes to the navbar so that the logo now takes the user to the homepage, added footer section and tested site for burger icon that would allow user to use chips and "add funds" to account. testing changes removed due to issue with button.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,18 @@
 /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
-function myFunction() {
+function burgerNav() {
     var x = document.getElementById("menu");
     if (x.style.display === "block") {
-      x.style.display = "none";
+        x.style.display = "none";
     } else {
-      x.style.display = "block";
+        x.style.display = "block";
     }
-  }
+}
+
+function burgerChip() {
+    var x = document.getElementById("menu");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
