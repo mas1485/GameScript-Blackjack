c4f878622b0df0adc52f27f9b49d126598558c62
Matthew Scott <matthew.scott@students.codeinstitute.net>
2024-10-16 18:12:07 +0100

    js
    set up array with the initial suit and value of the cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,21 @@
-/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+// wait for the dom the finish loading before running the game
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons){
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "stand") {
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        })
+    }
+})
+
+// Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu or bar icon
 function burgerNav() {
     var x = document.getElementById("menu");
     if (x.style.display === "block") {
@@ -16,3 +33,21 @@ function burgerChip() {
         x.style.display = "block";
     }
 }
+
+// randomly selects four cards using the value and suit
+
+function runGame() {
+    
+}
+
+const suit = ['Hearts', 'Diamonds', 'Spades', 'Clubs'];
+const value = [];
+
+for (let i = 1 ; i < 14 ; i++) {
+    value.push (i);
+}
+
+const randomSuit = suit[Math.floor(Math.random() * suit.length)];
+const randomValue = value[Math.floor(Math.random() * value.length)];
+const card =  randomValue + ' of ' + randomSuit;
+console.log(card);
