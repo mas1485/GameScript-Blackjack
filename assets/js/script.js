603d96eb3aa56abeb11c1b25926c74ae098f2a5d
Matthew Scott <matthew.scott@students.codeinstitute.net>
2024-10-17 18:23:04 +0100

    run game
    added array of objects for the cards and set up functions so that the game starts with four random cards from the deck and at each card draw the card is removed from the deck so there are no duplicates.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-// wait for the dom the finish loading before running the game
+value:// wait for the dom the finish loading before running the game
 
 document.addEventListener("DOMContentLoaded", function() {
     let buttons = document.getElementsByTagName("button");
@@ -9,10 +9,12 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("You clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         })
     }
+
+    runGame("blackjack");
 })
 
 // Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu or bar icon
@@ -36,18 +38,415 @@ function burgerChip() {
 
 // randomly selects four cards using the value and suit
 
-function runGame() {
-    
+function runGame(gameType) {
+
+    var newDeck = [...deck] ;
+
+    var card1 = getCardFromDeck(newDeck) ;
+    newDeck = removeCardFromDeck(newDeck , card1) ;
+
+    var card2 = getCardFromDeck(newDeck) ;
+    newDeck = removeCardFromDeck(newDeck , card2) ;
+
+    var card3 = getCardFromDeck(newDeck) ;
+    newDeck = removeCardFromDeck(newDeck , card3) ;
+
+    var card4 = getCardFromDeck(newDeck) ;
+    newDeck = removeCardFromDeck(newDeck , card4) ;
+
+    if (gameType === "blackjack") {
+        dealCards(card1, card2, card3, card4);
+    } else {
+        alert (`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
+
 }
 
-const suit = ['Hearts', 'Diamonds', 'Spades', 'Clubs'];
-const value = [];
-
-for (let i = 1 ; i < 14 ; i++) {
-    value.push (i);
+function getCardFromDeck(deck) {
+    var card = deck[Math.floor(Math.random() * deck.length)];
+    return card;
 }
 
-const randomSuit = suit[Math.floor(Math.random() * suit.length)];
-const randomValue = value[Math.floor(Math.random() * value.length)];
-const card =  randomValue + ' of ' + randomSuit;
-console.log(card);
+function removeCardFromDeck(deck , cardToRemove) {
+    deck = deck.filter((card) => card.id !== cardToRemove.id);
+    return deck;
+}
+
+function dealCards(card1, card2, card3, card4) {
+    document.getElementById('operand1').innerHTML = card1.name + " of " + card1.suit;
+    document.getElementById('operand2').innerHTML = card2.name + " of " + card2.suit;
+    document.getElementById('operand3').innerHTML = card3.name + " of " + card3.suit;
+    document.getElementById('operand4').innerHTML = card4.name + " of " + card4.suit;
+}
+
+function checkAnswer(operand1, operand2) {
+    let sum = operand1 + operand2;
+    return sum;
+}
+
+const deck = [
+    {
+        suit: "hearts",
+        name: "ace",
+        value: "1",
+        id: "1"
+    },
+
+    {
+        suit: "hearts",
+        name: "two",
+        value: "2",
+        id: "2"
+    },
+
+    {
+        suit: "hearts",
+        name: "three",
+        value: "3",
+        id: "3"
+    },
+
+    {
+        suit: "hearts",
+        name: "four",
+        value: "4",
+        id: "4"
+    },
+
+    {
+        suit: "hearts",
+        name: "five",
+        value: "5",
+        id: "5"
+    },
+
+    {
+        suit: "hearts",
+        name: "six",
+        value: "6",
+        id: "6"
+    },
+
+    {
+        suit: "hearts",
+        name: "seven",
+        value: "7",
+        id: "7"
+    },
+
+    {
+        suit: "hearts",
+        name: "eight",
+        value: "8",
+        id: "8"
+    },
+
+    {
+        suit: "hearts",
+        name: "nine",
+        value: "9",
+        id: "9"
+    },
+
+    {
+        suit: "hearts",
+        name: "ten",
+        value: "10",
+        id: "10"
+    },
+
+    {
+        suit: "hearts",
+        name: "jack",
+        value: "10",
+        id: "11"
+    },
+
+    {
+        suit: "hearts",
+        name: "queen",
+        value: "10",
+        id: "12"
+    },
+
+    {
+        suit: "hearts",
+        name: "king",
+        value: "10",
+        id: "13"
+    },
+
+    {
+        suit: "diamonds",
+        name: "ace",
+        value: "1",
+        id: "14"
+    },
+
+    {
+        suit: "diamonds",
+        name: "two",
+        value: "2",
+        id: "15"
+    },
+
+    {
+        suit: "diamonds",
+        name: "three",
+        value: "3",
+        id: "16"
+    },
+
+    {
+        suit: "diamonds",
+        name: "four",
+        value: "4",
+        id: "17"
+    },
+
+    {
+        suit: "diamonds",
+        name: "five",
+        value: "5",
+        id: "18"
+    },
+
+    {
+        suit: "diamonds",
+        name: "six",
+        value: "6",
+        id: "19"
+    },
+
+    {
+        suit: "diamonds",
+        name: "seven",
+        value: "7",
+        id: "20"
+    },
+
+    {
+        suit: "diamonds",
+        name: "eight",
+        value: "8",
+        id: "21"
+    },
+
+    {
+        suit: "diamonds",
+        name: "nine",
+        value: "9",
+        id: "22"
+    },
+
+    {
+        suit: "diamonds",
+        name: "ten",
+        value: "10",
+        id: "23"
+    },
+
+    {
+        suit: "diamonds",
+        name: "jack",
+        value: "10",
+        id: "24"
+    },
+
+    {
+        suit: "diamonds",
+        name: "queen",
+        value: "10",
+        id: "25"
+    },
+
+    {
+        suit: "diamonds",
+        name: "king",
+        value: "10",
+        id: "26"
+    },
+
+    {
+        suit: "spades",
+        name: "ace",
+        value: "1",
+        id: "27"
+    },
+
+    {
+        suit: "spades",
+        name: "two",
+        value: "2",
+        id: "28"
+    },
+
+    {
+        suit: "spades",
+        name: "three",
+        value: "3",
+        id: "29"
+    },
+
+    {
+        suit: "spades",
+        name: "four",
+        value: "4",
+        id: "30"
+    },
+
+    {
+        suit: "spades",
+        name: "five",
+        value: "5",
+        id: "31"
+    },
+
+    {
+        suit: "spades",
+        name: "six",
+        value: "6",
+        id: "32"
+    },
+
+    {
+        suit: "spades",
+        name: "seven",
+        value: "7",
+        id: "33"
+    },
+
+    {
+        suit: "spades",
+        name: "eight",
+        value: "8",
+        id: "34"
+    },
+
+    {
+        suit: "spades",
+        name: "nine",
+        value: "9",
+        id: "35"
+    },
+
+    {
+        suit: "spades",
+        name: "ten",
+        value: "10",
+        id: "36"
+    },
+
+    {
+        suit: "spades",
+        name: "jack",
+        value: "10",
+        id: "37"
+    },
+
+    {
+        suit: "spades",
+        name: "queen",
+        value: "10",
+        id: "38"
+    },
+
+    {
+        suit: "spades",
+        name: "king",
+        value: "10",
+        id: "39"
+    },
+
+    {
+        suit: "clubs",
+        name: "ace",
+        value: "1",
+        id: "40"
+    },
+
+    {
+        suit: "clubs",
+        name: "two",
+        value: "2",
+        id: "41"
+    },
+
+    {
+        suit: "clubs",
+        name: "three",
+        value: "3",
+        id: "42"
+    },
+
+    {
+        suit: "clubs",
+        name: "four",
+        value: "4",
+        id: "43"
+    },
+
+    {
+        suit: "clubs",
+        name: "five",
+        value: "5",
+        id: "44"
+    },
+
+    {
+        suit: "clubs",
+        name: "six",
+        value: "6",
+        id: "45"
+    },
+
+    {
+        suit: "clubs",
+        name: "seven",
+        value: "7",
+        id: "46"
+    },
+
+    {
+        suit: "clubs",
+        name: "eight",
+        value: "8",
+        id: "47"
+    },
+
+    {
+        suit: "clubs",
+        name: "nine",
+        value: "9",
+        id: "48"
+    },
+
+    {
+        suit: "clubs",
+        name: "ten",
+        value: "10",
+        id: "49"
+    },
+
+    {
+        suit: "clubs",
+        name: "jack",
+        value: "10",
+        id: "50"
+    },
+
+    {
+        suit: "clubs",
+        name: "queen",
+        value: "10",
+        id: "51"
+    },
+
+    {
+        suit: "clubs",
+        name: "king",
+        value: "10",
+        id: "52"
+    },
+]
